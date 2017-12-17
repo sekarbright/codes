b385d1c65d1e4bac61b9243050f612d8488ee124
rajasekar <sekarbright@gmail.com>
2017-12-17 10:51:07 +0530

    first commit

---
+++ colorleft_to_right.php
@@ -0,0 +1,21 @@
+<?php
+/*
+refer: http://gradcolor.com/css3-gradient.php?c=3
+*/
+?>
+<html><head>
+<style type="text/css">
+.css3gradient{width:600px;height:20px;
+ background-color:#ff0000;
+ filter:progid:DXImageTransform.Microsoft.gradient(GradientType=1,startColorstr=#ff0000, endColorstr=#ffff00);
+ background-image:-moz-linear-gradient(left, #ff0000 0%, #ffff00 50%,#008000 100%);
+background-image:linear-gradient(left, #ff0000 0%, #ffff00 50%,#008000 100%);
+background-image:-webkit-linear-gradient(left, #ff0000 0%, #ffff00 50%,#008000 100%);
+background-image:-o-linear-gradient(left, #ff0000 0%, #ffff00 50%,#008000 100%);
+background-image:-ms-linear-gradient(left, #ff0000 0%, #ffff00 50%,#008000 100%);
+ background-image:-webkit-gradient(linear, left bottom, right bottom, color-stop(0%,#ff0000), color-stop(50%,#ffff00),color-stop(100%,#008000));}
+</style>
+</head><body>
+<div class="css3gradient"><b>Here is the result</b></div>
+</body>
+</html>
